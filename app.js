e454a809f579afdcafbad0a5a5b6d30ea8171cc1
Opara Prosper <oparaprosper79@gmail.com>
2019-11-04 21:04:26 +0100

    Add resource for getting specific students

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ app.use('/svg/students', (req, res, next) => {
   res.status(200).json(db);
 })
 
+app.get('/students/:id', (req, res, next) => {
+  let user = Number(req.params.id);
+  
+  if( user === db[user - 1].id){
+    res.status(200).json(db[user - 1]);
+  }else{
+    res.status(404).json({
+      status: "there was an error",
+      message: 'student with such id does not exits'
+    })
+  }
+})
+
 app.use('/*', (req, res, next) => {
   res.status(400).json({
     error: 'resource not found'
