b75715106a69d70bff789bd367ade3515d240c18
Opara Prosper <oparaprosper79@gmail.com>
2019-11-05 19:39:58 +0100

    Fix lint errors

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
+/* eslint-disable no-console */
+/* eslint-disable no-restricted-globals */
 const http = require('http');
 const app = require('./app');
 
-const normalizePort = val => {
+const normalizePort = (val) => {
   const port = parseInt(val, 10);
 
-  if(isNaN(port)){
+  if (isNaN(port)) {
     return val;
   }
-  if(port >= 0){
+  if (port >= 0) {
     return port;
   }
 
   return false;
-}
+};
 
-const errorHandler = error => {
-  if(error.syscall !== 'listen'){
+const port = normalizePort(process.env.PORT || 3000);
+app.set('port', port);
+const server = http.createServer(app);
+
+const errorHandler = (error) => {
+  if (error.syscall !== 'listen') {
     throw error;
   }
 
   const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe' + address : 'port' + port;
+  const bind = typeof address === 'string' ? `pipe ${address}` : `port ${port}`;
 
-  switch(error.code){
+  switch (error.code) {
     case 'EACCES':
       console.error(bind, ' requires elevated priviledges');
       process.exit(1);
@@ -36,15 +42,11 @@ const errorHandler = error => {
   }
 };
 
-const port = normalizePort(process.env.PORT || 3000);
-app.set('port', port);
-const server = http.createServer(app);
-
-server.on('error', errorHandler)
+server.on('error', errorHandler);
 server.on('listening', () => {
   const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+  const bind = typeof address === 'string' ? `pipe ${address}` : `port ${port}`;
+  console.log(`Listening on ${bind}`);
 });
 
 
