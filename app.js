d324ac84126313454d17e574960af9f1b8fb1ad1
Opara Prosper <oparaprosper79@gmail.com>
2019-11-05 09:55:40 +0100

    Add proper error/success request messages

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const db = require('./db').default;
 
 const app = express();
 
-app.use('/svg/students', (req, res, next) => {
-  res.status(200).json(db);
+app.get('/students', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    students: db
+  });
 })
 
 app.get('/students/:id', (req, res, next) => {
@@ -13,7 +16,10 @@ app.get('/students/:id', (req, res, next) => {
   
   if(db[user-1]){
     if(user === db[user-1].id){
-      res.status(200).json(db[user - 1]);
+      res.status(200).json({
+        status: 'success',
+        student: db[user - 1]
+      });
     }
   }else{
     res.status(404).json({
