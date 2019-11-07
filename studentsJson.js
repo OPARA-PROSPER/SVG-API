f819fab44319f2d8dd6f0769eb5f24426249db1b
Opara Prosper <oparaprosper79@gmail.com>
2019-11-07 09:46:15 +0100

    Add students collection dummy data

---
+++ studentsJson.js
@@ -0,0 +1,53 @@
+// eslint-disable-next-line no-undef
+db.students.insert([
+  {
+    name: 'opara prosper',
+    reg_no: '20151010054',
+    age: 21,
+  },
+  {
+    name: 'opara prosper',
+    reg_no: '20151010054',
+    age: 21,
+  },
+  {
+    name: 'opara prosper',
+    reg_no: '20151010054',
+    age: 21,
+  },
+  {
+    name: 'opara prosper',
+    reg_no: '20151010054',
+    age: 21,
+  },
+  {
+    name: 'opara prosper',
+    reg_no: '20151010054',
+    age: 21,
+  },
+  {
+    name: 'opara prosper',
+    reg_no: '20151010054',
+    age: 21,
+  },
+  {
+    name: 'opara prosper',
+    reg_no: '20151010054',
+    age: 21,
+  },
+  {
+    name: 'opara prosper',
+    reg_no: '20151010054',
+    age: 21,
+  },
+  {
+    name: 'opara prosper',
+    reg_no: '20151010054',
+    age: 21,
+  },
+  {
+    name: 'opara prosper',
+    reg_no: '20151010054',
+    age: 21,
+  },
+]);
