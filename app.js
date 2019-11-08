b7f13de7348269a614e4bc3c08657ab40a50727a
Opara Prosper <oparaprosper79@gmail.com>
2019-11-08 17:59:18 +0100

    Add support for cross origin resource sharing

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const mongoose = require('mongoose');
 const Student = require('./models/students');
 
 mongoose.connect('mongodb://localhost/svg-api');
-
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.get('/students', (req, res) => {
   Student.find().then((students) => {
